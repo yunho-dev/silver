662e151fa3000c498d19e1b8909e6e352eef294a
oh96 <gad03962@gmail.com>
2023-01-24 15:43:50 +0900

    조직도수정

--- src/main/java/com/silver/member/MemberOrgChartController.java
+++ src/main/java/com/silver/member/MemberOrgChartController.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,7 +18,12 @@ public class MemberOrgChartController {
 	
 	
 	@GetMapping(value = "/memberOrgChart")
-	public ModelAndView donation() {
+	public ModelAndView donation(Model model,@RequestParam HashMap<String, String> params) {
+		logger.info("조직도 조회");
+		
+		logger.info("세션 값 조회");
+		logger.info("params:{}",params);
+		model.addAttribute("page", params);
 		ModelAndView mav = new ModelAndView("member/memberorgchartList");
 		return mav;
 	}
